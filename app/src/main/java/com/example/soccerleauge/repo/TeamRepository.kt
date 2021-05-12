a7121e75d861ef7e35eb9ca29d099d4f523af79c
Mucahid <mucahidd3@gmail.com>
2021-05-12 22:35:19 +0300

    Added repository

--- app/src/main/java/com/example/soccerleauge/repo/TeamRepository.kt
+++ app/src/main/java/com/example/soccerleauge/repo/TeamRepository.kt
@@ -1,4 +1,17 @@
 package com.example.soccerleauge.repo
 
-class TeamRepository {
+import com.example.soccerleauge.BaseRepository
+import com.example.soccerleauge.model.Teams
+import com.example.soccerleauge.network.Api
+import com.example.soccerleauge.util.Resource
+import javax.inject.Inject
+
+class TeamRepository @Inject constructor(private val api: Api) : BaseRepository(){
+
+    suspend fun getTeams() : Resource<Teams>{
+        return getResult {
+            api.getTeams()
+        }
+    }
+
 }
