6f3edcbb09baa209e9319f5e13406038e9b88f59
Mucahid <mucahidd3@gmail.com>
2021-05-12 23:26:53 +0300

    Added TeamListViewModel.kt

--- app/src/main/java/com/example/soccerleauge/ui/MainActivity.kt
+++ app/src/main/java/com/example/soccerleauge/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.soccerleauge
+package com.example.soccerleauge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.soccerleauge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/soccerleauge/ui/viewmodel/TeamListViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.soccerleauge.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.soccerleauge.model.TeamInfo
+import com.example.soccerleauge.repo.TeamRepository
+import com.example.soccerleauge.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TeamListViewModel @Inject constructor(val repository: TeamRepository) : ViewModel() {
+
+    val team = MutableLiveData<Resource<List<TeamInfo>>>()
+
+    fun getTeamData(){
+        viewModelScope.launch {
+            team.postValue(Resource.loading())
+
+            repository.getTeams().let {
+                team.postValue(Resource.succes(it.data?.teams.orEmpty()))
+            }
+        }
+    }
+
+}
