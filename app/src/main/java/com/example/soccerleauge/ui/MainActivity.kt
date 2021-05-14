46faedcd4c362d7222c107d492bad1eef2a587dc
Mucahid <mucahidd3@gmail.com>
2021-05-14 12:00:54 +0300

    Created SettingFragment.kt

--- app/src/main/java/com/example/soccerleauge/ui/MainActivity.kt
+++ app/src/main/java/com/example/soccerleauge/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.soccerleauge.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -11,6 +13,8 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.soccerleauge.R
+import com.example.soccerleauge.ui.settings.SettingFragment
+import com.example.soccerleauge.ui.teamlist.TeamListFragment
 import com.example.soccerleauge.ui.viewmodel.TeamListViewModel
 import com.example.soccerleauge.util.Status
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +24,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController : NavController
+    private val fragmentSettings = SettingFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +32,29 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         navController = navHostFragment.navController
-
-//        navController = Navigation.findNavController(this,R.id.fragment)
         NavigationUI.setupActionBarWithNavController(this,navController)
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController,null)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settins_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+        if (id == R.id.settings){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment,fragmentSettings)
+                .addToBackStack(TeamListFragment::class.simpleName)
+                .commit()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
--- app/src/main/java/com/example/soccerleauge/ui/teamlist/TeamListFragment.kt
+++ app/src/main/java/com/example/soccerleauge/ui/teamlist/TeamListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.soccerleauge.ui.teamlist
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
@@ -80,5 +78,6 @@ class TeamListFragment : Fragment() {
             }
         })
     }
+
 
 }
